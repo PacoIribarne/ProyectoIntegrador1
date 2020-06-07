7dab41cafb670bd33517ac564242b8baa0767ae4
AgustinIhidoype <aihidoype@udesa.edu.ar>
2020-06-06 21:00:55 -0300

    dveve

--- js/detailgenre.js
+++ js/detailgenre.js
@@ -1,25 +1,41 @@
-// let queryString = location.search;
-// let datos = new URLSearchParams(queryString);
-// let idGenre = datos.get("id");
+let queryString = location.search;
+let datos = new URLSearchParams(queryString);
+let idGenre = datos.get("id");
 
-// let proxy = "https://cors-anywhere.herokuapp.com/";
-// let urlGenre = proxy + "https://api.deezer.com/genre/" + idGenre;
+let proxy = "https://cors-anywhere.herokuapp.com/";
+let urlGenre = proxy + "https://api.deezer.com/genre/" + idGenre;
 
-// fetch(urlGenre)
-//     .then(function(response){
-//     return response.json();
-// })
-//     .then(function(datos){
-//     console.log(datos);
-//     let nombreGenero = document.querySelector(".NombreGenero");
-//     nombreGenero.innerHTML += datos.genre_id;
-// })
-// .catch(function(error){
-//     console.log(error)
-// })
+fetch(urlGenre)
+    .then(function(response){
+    return response.json();
+    })
+        .then(function(datos){
+        console.log(datos);
+        let nombreGenero = document.querySelector(".NombreGenero");
+        nombreGenero.innerHTML += datos.name;
+    })
+    .catch(function(error){
+        console.log(error)
+    })
 
 
-// let urlAlbum = proxy + "https://api.deezer.com/album/" + idGenre;
+let urlGenreArtist = proxy + "https://api.deezer.com/genre/" + idGenre + "/artists";
+
+fetch(urlGenreArtist)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos);
+        let resultados = datos.data;
+        let artists = document.querySelector('.artists');
+        resultados.forEach(function(artist){
+        artists.innerHTML += '<a class="artist "href="'+'"> <img src="' + artist.picture_medium + '"> <p class="nombre">' + artist.name + '</p> </a>'
+        })
+    })
+    .catch(function(error){
+        console.log(error)
+    })
 
 // fetch(urlAlbum)
 //     .then(function(response){
--- js/generos.js
+++ js/generos.js
@@ -1,10 +1,5 @@
-let queryString = location.search;
-let datos = new URLSearchParams(queryString);
-let idTrack = datos.get('id');
-
 let proxi = 'https://cors-anywhere.herokuapp.com/';
 let url = proxi + 'https://api.deezer.com/genre';
-
 
 fetch(url)
     .then(function(response){
@@ -14,9 +9,8 @@ fetch(url)
         let generos = document.querySelector('.generos');
         console.log(datos);
         let resultados = datos.data;
-
         resultados.forEach(function(genres){
-            generos.innerHTML += "<div class='portadagenero'>" + "<a href='detailgenre.html?id=" + genres.genre_id + "'>" +"<img src='" + genres.picture_medium  +  "'>" + genres.name + "</div>";
+            generos.innerHTML += '<div class="portadagenero">' + '<a href="detailgenre.html?id=' + genres.id + '">' +'<img src="'+ genres.picture_medium  +  '">' + genres.name + '</div>';
         })
     })
     .catch(function(error){
