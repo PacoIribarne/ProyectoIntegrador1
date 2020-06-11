0c1b2fdd5215684fcccee70d50c205252a53549b
AgustinIhidoype <aihidoype@udesa.edu.ar>
2020-06-11 11:02:07 -0300

    wwww

--- js/detailalbum.js
+++ js/detailalbum.js
@@ -1,3 +1,73 @@
+let queryString = location.search;
+let datos = new URLSearchParams(queryString);
+let idAlbum = datos.get("id");
+
+let proxy = "https://cors-anywhere.herokuapp.com/";
+let urlGenre = proxy + "https://api.deezer.com/album/" + idAlbum;
+
+fetch(urlGenre)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        console.log(datos)
+        let infoAlbum = document.querySelector('.infoAlbum');
+        infoAlbum.innerHTML += '<h1>' + datos.title + '</h1>';
+        infoAlbum.innerHTML += '<a href="detailartist.html?id=' + datos.artist.id + '"> <h2>' + datos.artist.name + '</h2> </a>';
+        infoAlbum.innerHTML += '<p class="lanzamiento">' + datos.release_date + '</p>';
+        infoAlbum.innerHTML += '<img class="imagenAlbum" src="' + datos.cover_big + '" alt="' + datos.title + '">'
+
+        let listado = document.querySelector('.listado');
+        for(let i=0 ; i < length ; i++){
+            listado.innerHTML += '<li> <iframe scrolling="no" frameborder="0" allowTransparency="true" width="700" height="350" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + datos.tracks.data[i].id + '&app_id=1"></iframe> </li>'
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+
+
+
+
+    
+    // <li class="cancion">
+                    
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 2</p>
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 3</p>
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 4</p>
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 5</p>
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 6</p>
+    // </li>
+    // <li class="cancion">
+    //     <img class="boton-play" src="../img/play.png" alt="">
+    //     <p class="nombre">Canción 7</p>
+    // </li>
+
+
+
+
+
+
+
+
+
+
+
 // let songs = ['../audio/spinetta.mp3'];
 // let poster = ['../img/album4spinetta.jpg'];
 
@@ -25,28 +95,28 @@
 // }
 
 //--------
-let cancion = document.querySelector('.cancion')
-let play = document.querySelector('#play')
-let otroPlay = document.querySelector('.boton-play')
-let audio = document.querySelector('.audioque')
+// let cancion = document.querySelector('.cancion')
+// let play = document.querySelector('#play')
+// let otroPlay = document.querySelector('.boton-play')
+// let audio = document.querySelector('.audioque')
 
-otroPlay.onclick = function(){
-    audio.src = '../audio/spinetta.mp3'
-    otroPlay.onclick = function(){
+// otroPlay.onclick = function(){
+//     audio.src = '../audio/spinetta.mp3'
+//     otroPlay.onclick = function(){
     
     
-    }
-}
+//     }
+// }
 
-play.onclick = function(){
-    if(song.paused){
-        song.play();
-        play.innerHTML = '<img src="../img/pause.png">';
-    }else{
-        song.pause();
-        play.innerHTML = '<img src="../img/play.png">';
-    }
-}
+// play.onclick = function(){
+//     if(song.paused){
+//         song.play();
+//         play.innerHTML = '<img src="../img/pause.png">';
+//     }else{
+//         song.pause();
+//         play.innerHTML = '<img src="../img/play.png">';
+//     }
+// }
 
 // ----------
 // function createAudioPlayer() {
@@ -91,3 +161,27 @@ play.onclick = function(){
 //     }
 
 // }
+// ----
+// <div class="sonar">
+//         <div class="infoTema">
+//             <div class="nombreTema">Spinetta</div>
+//             <div class="nombreArista">Spinetta</div>
+//         </div>
+//         <div class="reproduccion">
+//             <div class="botones">
+//                 <img  id="pre" src="../img/pre.png" alt="">
+//                 <img id="play" src="../img/play.png" alt="">
+//                 <img id="next" src="../img/next.png" alt="">
+//             </div>
+//             <div class="tiempo">
+//                 <div class="seekbar">
+//                     <div id="fill" ></div>
+//                     <div id="handle" ></div>
+//                 </div>
+//                 <div class="numeros">
+//                     <p class="tiempito"></p>
+//                 </div>
+//             </div>
+//             <audio class="audioque" src=""></audio>
+//         </div>
+//     </div>
