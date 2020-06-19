0a756ca181db9155e00f20986ea4e19648658bdc
AgustinIhidoype <aihidoype@udesa.edu.ar>
2020-06-18 21:12:53 -0300

    3523

--- js/index.js
+++ js/index.js
@@ -47,34 +47,15 @@ fetch(urlChart)
       //   albumblock.innerHTML += '<a href= class="portadaAlbum" <img src="' + album.cover + '"> <p>' + album.title + '-' + album.artist.name + '</p> </a>'
       // })
       for (let i = 0; i < 4; i++){
-        abajo.innerHTML += '<a class="albumblock" href="detailtrack.html?id=' + datos.tracks.data[i].id + '"> <img class="portadaTrack" src="' + datos.tracks.data[i].album.cover + '"> <p>' + datos.tracks.data[i].title + ' - ' + datos.tracks.data[i].artist.name + '</p> </a>'
+        abajo.innerHTML += '<a class="albumblock" href="detailtrack.html?id=' + datos.tracks.data[i].id + '"> <img class="portadaTrack" src="' + datos.tracks.data[i].album.cover + '"> <p class="muyBueno">' + datos.tracks.data[i].title + ' - ' + datos.tracks.data[i].artist.name + '</p> </a>'
       }
       for (let i = 0; i < 4; i++){
-        abajo2.innerHTML += '<a class="albumblock" href="detailalbum.html?id=' + datos.albums.data[i].id + '"> <img class="portadaAlbum" src="' + datos.albums.data[i].cover_medium + '"> <p>' + datos.albums.data[i].title + ' - ' + datos.albums.data[i].artist.name + '</p> </a>'
+        abajo2.innerHTML += '<a class="albumblock" href="detailalbum.html?id=' + datos.albums.data[i].id + '"> <img class="portadaAlbum" src="' + datos.albums.data[i].cover_medium + '"> <p class="muyBueno">' + datos.albums.data[i].title + ' - ' + datos.albums.data[i].artist.name + '</p> </a>'
       }
       for (let i = 0; i < 4; i++){
-        abajo3.innerHTML += '<a class="albumblock" href="detailartist.html?id=' + datos.artists.data[i].id + '"> <img class="portadaArtist" src="' + datos.artists.data[i].picture + '"> <p>' + datos.artists.data[i].name + '</p> </a>'
+        abajo3.innerHTML += '<a class="albumblock" href="detailartist.html?id=' + datos.artists.data[i].id + '"> <img class="portadaArtist" src="' + datos.artists.data[i].picture + '"> <p class="muyBueno">' + datos.artists.data[i].name + '</p> </a>'
       }
   })
   .catch(function(error){
     console.log(error)
 })
-
-
-// <a href="detailalbum.html"><div class="albumblock">
-//                 <img class="portadaAlbum" height="100px" >
-//                 <p class="nombrecancion">The Getaway | Red Hot Chilli Peppers </p>
-//             </div></a>
-//             <a href="detailalbum.html"><div class="albumblock">
-//                 <img class="portadaAlbum" height="100px" >
-//                 <p class="nombrecancion">The Getaway | Red Hot Chilli Peppers </p>
-//             </div></a>
-//             <a href="detailalbum.html"><div class="albumblock">
-//                 <img class="portadaAlbum" height="100px" >
-//                 <p class="nombrecancion">The Getaway | Red Hot Chilli Peppers </p>
-//             </div></a>
-//             <a href="detailalbum.html"><div class="albumblock">
-//                 <img class="portadaAlbum" height="100px" >
-//                 <p class="nombrecancion">The Getaway | Red Hot Chilli Peppers </p>
-//             </div></a>
-
